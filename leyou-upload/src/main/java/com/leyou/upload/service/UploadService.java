cbda6e90432e8af8a7bc908d390b6b756d80b0d1
YuDongZhang <977872239@qq.com>
2021-08-02 17:12:25 +0800

    18.重新修改上传的路径

--- leyou-upload/src/main/java/com/leyou/upload/service/UploadService.java
+++ leyou-upload/src/main/java/com/leyou/upload/service/UploadService.java
@@ -1,8 +1,12 @@
 package com.leyou.upload.service;
 
 
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,6 +20,9 @@ import java.util.List;
 
 @Service
 public class UploadService {
+
+    @Autowired
+    private FastFileStorageClient storageClient;
 
     //常量没有办法new , 解决方案 , 有效的文件类型
     private static final List<String> CONTENT_TYPES = Arrays.asList("image/jpeg", "image/gif");//自己去添加更多类型
@@ -43,10 +50,12 @@ public class UploadService {
             }
 
             // 保存到服务器
-            file.transferTo(new File("C:\\WrokSpace\\leyou\\images\\" + originalFilename));
-
+//            file.transferTo(new File("C:\\WrokSpace\\leyou\\images\\" + originalFilename));
+            String ext = StringUtils.substringAfterLast(originalFilename, ".");
+            StorePath storePath = this.storageClient.uploadFile(file.getInputStream(), file.getSize(), ext, null);
             // 生成url地址，返回
-            return "http://image.leyou.com/" + originalFilename;
+//            return "http://image.leyou.com/" + originalFilename;
+            return "http://image.leyou.com/" + storePath.getFullPath();
         } catch (IOException e) {
             LOGGER.info("服务器内部错误：{}", originalFilename);
             e.printStackTrace();
