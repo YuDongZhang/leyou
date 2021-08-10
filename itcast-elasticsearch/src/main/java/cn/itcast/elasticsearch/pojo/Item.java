a99a451b9b781423524ced85a0e104a36c68635a
YuDongZhang <977872239@qq.com>
2021-08-10 14:37:48 +0800

    29.elasticsearch索引创建成功,要在centos端启动 ./elasticsearch , yml文件中要改成centos的ip,这个是配合之前的,在kabana中可以查到 创建的索引

--- itcast-elasticsearch/src/main/java/cn/itcast/elasticsearch/pojo/Item.java
+++ itcast-elasticsearch/src/main/java/cn/itcast/elasticsearch/pojo/Item.java
@@ -1,12 +1,30 @@
 package cn.itcast.elasticsearch.pojo;
 
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Document(indexName = "item",type = "docs", shards = 1, replicas = 0)
 public class Item {
-    Long id;
-    String title; //标题
-    String category;// 分类
-    String brand; // 品牌
-    Double price; // 价格
-    String images; // 图片地址
+    @Id
+    private Long id;
+
+    @Field(type = FieldType.Text, analyzer = "ik_max_word")
+    private String title; //标题
+
+    @Field(type = FieldType.Keyword)
+    private String category;// 分类
+
+    @Field(type = FieldType.Keyword)
+    private String brand; // 品牌
+
+    @Field(type = FieldType.Double)
+    private Double price; // 价格
+
+    @Field(index = false, type = FieldType.Keyword)
+    private String images; // 图片地址
 
     public Long getId() {
         return id;
@@ -56,3 +74,4 @@ public class Item {
         this.images = images;
     }
 }
+
