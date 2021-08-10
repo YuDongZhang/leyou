168c0a639e12cc0ef407964f74146efd3863cdc1
YuDongZhang <977872239@qq.com>
2021-08-10 15:26:11 +0800

    31.elasticsearch  自定义方法查询

--- itcast-elasticsearch/src/main/java/cn/itcast/elasticsearch/ItemRepository.java
+++ itcast-elasticsearch/src/main/java/cn/itcast/elasticsearch/ItemRepository.java
@@ -3,5 +3,14 @@ package cn.itcast.elasticsearch;
 import cn.itcast.elasticsearch.pojo.Item;
 import org.springframework.data.elasticsearch.repository.ElasticsearchCrudRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends ElasticsearchCrudRepository<Item,Long> {//Long为对象主键
+    /**
+     * 根据价格区间查询
+     * @param price1
+     * @param price2
+     * @return
+     */
+    List<Item> findByPriceBetween(double price1, double price2);
 }
--- itcast-elasticsearch/src/test/java/IndexTest.java
+++ itcast-elasticsearch/src/test/java/IndexTest.java
@@ -39,12 +39,21 @@ public class IndexTest {
         itemRepository.save(item);*/
 
 
-        List<Item> list = new ArrayList<>();
+    /*    List<Item> list = new ArrayList<>();
         list.add(new Item(2L, "坚果手机R1", " 手机", "锤子", 3699.00, "http://image.leyou.com/123.jpg"));
         list.add(new Item(3L, "华为META10", " 手机", "华为", 4499.00, "http://image.leyou.com/3.jpg"));
         // 接收对象集合，实现批量新增
-        itemRepository.saveAll(list);
+        itemRepository.saveAll(list);*/
 //        itemRepository.delete();
+
+        List<Item> list = new ArrayList<>();
+        list.add(new Item(1L, "小米手机7", "手机", "小米", 3299.00, "http://image.leyou.com/13123.jpg"));
+        list.add(new Item(2L, "坚果手机R1", "手机", "锤子", 3699.00, "http://image.leyou.com/13123.jpg"));
+        list.add(new Item(3L, "华为META10", "手机", "华为", 4499.00, "http://image.leyou.com/13123.jpg"));
+        list.add(new Item(4L, "小米Mix2S", "手机", "小米", 4299.00, "http://image.leyou.com/13123.jpg"));
+        list.add(new Item(5L, "荣耀V10", "手机", "华为", 2799.00, "http://image.leyou.com/13123.jpg"));
+        // 接收对象集合，实现批量新增
+        itemRepository.saveAll(list);
     }
 
 
@@ -59,6 +68,14 @@ public class IndexTest {
         // 查询全部，并按照价格降序排序
         Iterable<Item> items = this.itemRepository.findAll(Sort.by(Sort.Direction.DESC, "price"));
         items.forEach(item-> System.out.println(item));
+    }
+
+    @Test
+    public void queryByPriceBetween(){
+        List<Item> list = this.itemRepository.findByPriceBetween(2000.00, 3500.00);
+        for (Item item : list) {
+            System.out.println("item = " + item);
+        }
     }
 
 }
