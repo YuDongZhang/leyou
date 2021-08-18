5711ef4e2f33fb69fb182b0f72e454ae9bd64069
YuDongZhang <977872239@qq.com>
2021-08-18 13:06:58 +0800

    53.day16 04.搭建短信微服务,现在没有短信可以用,主要可以在 rabbitqm查到队列,这里没有运行成功,application中缺少注解

---
+++ leyou-sms/src/main/java/com/leyou/LeyouSmsApplication.java
@@ -0,0 +1,9 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+
+public class LeyouSmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouSmsApplication.class);
+    }
+}
