0659feb0dae7c1cb386d71be2e772a3974171cba
YuDongZhang <977872239@qq.com>
2021-08-18 11:16:35 +0800

    52.day16 01.数据是否可用校验测试成功

---
+++ leyou-user/leyou-user-service/src/main/java/com/leyou/user/service/UserService.java
@@ -0,0 +1,34 @@
+package com.leyou.user.service;
+
+import com.leyou.user.mapper.UserMapper;
+import com.leyou.user.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    /**
+     * 校验数据是否可用
+     * @param data
+     * @param type
+     * @return
+     */
+    public Boolean checkData(String data, Integer type) {
+        User record = new User();
+        switch (type) {
+            case 1:
+                record.setUsername(data);
+                break;
+            case 2:
+                record.setPhone(data);
+                break;
+            default:
+                return null;
+        }
+        return this.userMapper.selectCount(record) == 0;
+    }
+}
