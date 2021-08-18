63971714f0f2caf68f5a77bcb5e4c3e922c06be9
YuDongZhang <977872239@qq.com>
2021-08-18 18:48:53 +0800

    56.day16 06用户注册功能,这里要停掉短信服务,在redis客户端,模拟数据user:verify:17621979982 123456 ,然后在postman localhost:8085/register,经过测试请求成功,tb_user中可以查看

--- leyou-user/leyou-user-service/src/main/java/com/leyou/user/controller/UserController.java
+++ leyou-user/leyou-user-service/src/main/java/com/leyou/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.leyou.user.controller;
 
+import com.leyou.user.pojo.User;
 import com.leyou.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -40,6 +42,21 @@ public class UserController {
         Boolean boo = this.userService.sendVerifyCode(phone);
         if (boo == null || !boo) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    /**
+     * 注册
+     * @param user
+     * @param code
+     * @return
+     */
+    @PostMapping("register")
+    public ResponseEntity<Void> register(User user, @RequestParam("code") String code) {
+        Boolean boo = this.userService.register(user, code);
+        if (boo == null || !boo) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
--- leyou-user/leyou-user-service/src/main/java/com/leyou/user/service/UserService.java
+++ leyou-user/leyou-user-service/src/main/java/com/leyou/user/service/UserService.java
@@ -3,6 +3,8 @@ package com.leyou.user.service;
 import com.leyou.common.utils.NumberUtils;
 import com.leyou.user.mapper.UserMapper;
 import com.leyou.user.pojo.User;
+import com.leyou.user.utils.CodecUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.AmqpTemplate;
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -48,7 +51,7 @@ public class UserService {
     @Autowired
     private AmqpTemplate amqpTemplate;
 
-    static final String KEY_PREFIX = "user:code:phone:";
+    static final String KEY_PREFIX = "user:verify:";
 
     static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -68,5 +71,32 @@ public class UserService {
             logger.error("发送短信失败。phone：{}， code：{}", phone, code);
             return false;
         }
+    }
+
+    public Boolean register(User user, String code) {
+        // 校验redis中的验证码
+        String cacheCode = this.redisTemplate.opsForValue().get(KEY_PREFIX + user.getPhone());
+        if (!StringUtils.equals(code, cacheCode)) {
+            return false;
+        }
+
+        // 生成盐
+        String salt = CodecUtils.generateSalt();
+        user.setSalt(salt);
+
+        // 对密码加密
+        user.setPassword(CodecUtils.md5Hex(user.getPassword(), salt));
+
+        // 强制设置不能指定的参数为null
+        user.setId(null);
+        user.setCreated(new Date());
+        // 添加到数据库
+        boolean b = this.userMapper.insertSelective(user) == 1;
+
+        if(b){
+            // 注册成功，删除redis中的记录
+            this.redisTemplate.delete(KEY_PREFIX + user.getPhone());
+        }
+        return b;
     }
 }
