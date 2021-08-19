f6e1d618d0dcf549765064cbd5ad891347daf3d4
YuDongZhang <977872239@qq.com>
2021-08-19 15:09:05 +0800

    63.day17 11.添加白名单,测试成功

--- leyou-gateway/src/main/java/com/leyou/gateway/filter/LoginFilter.java
+++ leyou-gateway/src/main/java/com/leyou/gateway/filter/LoginFilter.java
@@ -2,10 +2,12 @@ package com.leyou.gateway.filter;
 
 import com.leyou.common.utils.CookieUtils;
 import com.leyou.common.utils.JwtUtils;
+import com.leyou.gateway.config.FilterProperties;
 import com.leyou.gateway.config.JwtProperties;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.http.HttpStatus;
@@ -14,11 +16,14 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 
 @Component
-@EnableConfigurationProperties(JwtProperties.class)
+@EnableConfigurationProperties({JwtProperties.class, FilterProperties.class})
 public class LoginFilter extends ZuulFilter {
 
     @Autowired
     private JwtProperties properties;
+
+    @Autowired
+    private FilterProperties filterProperties;
 
     @Override
     public String filterType() {
@@ -32,6 +37,24 @@ public class LoginFilter extends ZuulFilter {
 
     @Override
     public boolean shouldFilter() {
+        // 获取上下文
+        RequestContext ctx = RequestContext.getCurrentContext();
+        // 获取request
+        HttpServletRequest req = ctx.getRequest();
+        // 获取路径
+        String requestURI = req.getRequestURI();
+        // 判断白名单
+        // 遍历允许访问的路径
+        for (String path : this.filterProperties.getAllowPaths()) {
+            // 然后判断是否是符合
+//            if(requestURI.startsWith(path)){
+//                return false;
+//            }
+            if (StringUtils.contains(requestURI,path)){//视频写法
+                return false;
+            }
+        }
+
         return true;
     }
 
