9a9f10821c2f6d7421df2b51ec8d06eb15048fff
YuDongZhang <977872239@qq.com>
2021-08-20 08:39:01 +0800

    67.day18 06.添加拦截器解析用户信息 07.登录状态购物车的结构 未运行

---
+++ leyou-cart/src/main/java/com/leyou/cart/interceptor/LoginInterceptor.java
@@ -0,0 +1,65 @@
+package com.leyou.cart.interceptor;
+
+import com.leyou.cart.config.JwtProperties;
+import com.leyou.common.pojo.UserInfo;
+import com.leyou.common.utils.CookieUtils;
+import com.leyou.common.utils.JwtUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * springmvc拦截器
+ */
+@Component
+@EnableConfigurationProperties(JwtProperties.class)
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    // 定义一个线程域，存放登录用户 , 每个请求都会分配线程 ,这个线程是贯穿的,而且里面的变量会自动的随threadlocal消失而消失
+    private static final ThreadLocal<UserInfo> tl = new ThreadLocal<>();
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 查询token
+        String token = CookieUtils.getCookieValue(request, this.jwtProperties.getCookieName());
+        if (StringUtils.isBlank(token)) {
+            // 未登录,返回401
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            return false;
+        }
+        // 有token，查询用户信息
+        try {
+            // 解析成功，证明已经登录
+            UserInfo user = JwtUtils.getInfoFromToken(token, jwtProperties.getPublicKey());
+            // 放入线程域
+            tl.set(user);
+            return true;
+        } catch (Exception e){
+            // 抛出异常，证明未登录,返回401
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            return false;
+        }
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //清空线程的局部变量,因为使用的 tomca的线程池 , 线程不会结束,就不会释放线程局部的变量
+        tl.remove();
+    }
+
+    //get是get里面的泛型,因为里面就是userinfo , 此方法 提供给别人用
+    public static UserInfo getLoginUser() {
+        return tl.get();
+    }
+}
